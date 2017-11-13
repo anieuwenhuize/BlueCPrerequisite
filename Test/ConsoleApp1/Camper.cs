b1fd557d856e5994cbe24d624d650879c9a32041
Andries <nieu0082@hz.nl>
2017-11-13 13:39:24 +0100

    Camperklasse erbij.

---
+++ Test/ConsoleApp1/Camper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Camper
+    {
+        private int AantalRamen;
+        private int AantalDeuren;
+        private bool Antenne;
+
+        public Camper( bool heeftAntenne )
+        {
+            this.AantalRamen = 4;
+            this.AantalDeuren = 3;
+            this.Antenne = heeftAntenne;
+        }
+
+        public bool HeeftAntenne()
+        {
+            return this.Antenne;
+        }
+
+        public int GeefAantalRamen()
+        {
+            return this.AantalRamen;
+        }
+        public void VoegEenRaamToe(int aantal)
+        {
+
+        }
+
+        public void VoegEenRaamToe()
+        {
+            this.AantalRamen += 1;
+            //this.AantalRamen = this.AantalRamen + 1;
+        }
+    }
+}
--- Test/ConsoleApp1/Program.cs
+++ Test/ConsoleApp1/Program.cs
@@ -6,19 +6,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Camper mijncamper = new Camper(true);
 
-            Huis mijnHuis = new Huis("Zeldenrust", 2);
-            Huis deburen = new Huis("Altijd lawaai", 1);
+            //Console.WriteLine(mijncamper.HeeftAntenne());
+
+            Console.WriteLine(mijncamper.GeefAantalRamen());
+            mijncamper.VoegEenRaamToe();
+
+            Console.WriteLine(mijncamper.GeefAantalRamen());
+
+            Console.Read();
+
+            //Huis mijnHuis = new Huis("Zeldenrust", 2);
+            //Huis deburen = new Huis("Altijd lawaai", 1);
             
-            // Dit gaat over mijn huis
-            Console.WriteLine(mijnHuis.GeefAantalRamen());
-            Console.WriteLine( mijnHuis.GeefWoonindicatie());
+            //// Dit gaat over mijn huis
+            //Console.WriteLine(mijnHuis.GeefAantalRamen());
+            //Console.WriteLine( mijnHuis.GeefWoonindicatie());
 
-            // dit gaat over de buurtjes
-            Console.WriteLine(deburen.GeefAantalDakgoten());
+            //// dit gaat over de buurtjes
+            //Console.WriteLine(deburen.GeefAantalDakgoten());
 
 
-            string name = Console.ReadLine();
+            //string name = Console.ReadLine();
             
 
             
